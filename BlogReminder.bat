11f4f9e109c78e8282016dec1df4fb68277f7463
ronitahuja <ahujaronit321@gmail.com>
2025-04-21 21:09:10 +0530

    updated and added venv

--- BlogReminder.bat
+++ BlogReminder.bat
@@ -1,2 +1,3 @@
 @echo off
+call "C:\Users\Ronit Ahuja\OneDrive\Desktop\SystemDesignReminder\venv\Scripts\activate.bat"
 python "C:\Users\Ronit Ahuja\OneDrive\Desktop\SystemDesignReminder\BlogReminder.pyw"
